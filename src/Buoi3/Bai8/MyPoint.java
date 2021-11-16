2ab1688efb61c0e95a6f467ae2dea5955dcb9311
samukma <nguyensam576@gmail.com>
2021-11-16 20:12:22 +0700

    fix

---
+++ src/Buoi3/Bai8/MyPoint.java
@@ -0,0 +1,32 @@
+package Buoi3.Bai8;
+
+public class MyPoint {
+    public int x;
+    public int y;
+
+    public MyPoint() {
+    }
+
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+
+}
