c25d79a698483a68dd8853547529b5c641180032
samukma <nguyensam576@gmail.com>
2021-11-23 00:31:57 +0700

    b5

---
+++ src/Buoi3/Bai5/Main5.java
@@ -0,0 +1,11 @@
+package Buoi3.Bai5;
+
+public class Main5 {
+    public static void main(String[] args) {
+        Date date = new Date();
+        date.setDay(23);
+        date.setMonth(11);
+        date.setYear(2021);
+        date.showDate();
+    }
+}
