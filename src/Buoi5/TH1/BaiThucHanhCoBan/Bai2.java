78eae950d79f57f85a83784da6f5f0c7a3e92331
samukma <nguyensam576@gmail.com>
2021-11-30 19:33:53 +0700

    buoi5.th1.thcb.b2

---
+++ src/Buoi5/TH1/BaiThucHanhCoBan/Bai2.java
@@ -0,0 +1,14 @@
+package Buoi5.TH1.BaiThucHanhCoBan;
+
+import java.util.Scanner;
+
+public class Bai2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input: ");
+        String input = scanner.nextLine();
+
+        System.out.println("Output: " + input);
+    }
+}
