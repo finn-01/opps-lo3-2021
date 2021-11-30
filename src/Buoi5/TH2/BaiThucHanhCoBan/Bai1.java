a95e6d7bb586ad87a02a8ea953546e004fa93de7
samukma <nguyensam576@gmail.com>
2021-11-30 19:59:02 +0700

    BaiTH2.bthcb.b1

---
+++ src/Buoi5/TH2/BaiThucHanhCoBan/Bai1.java
@@ -0,0 +1,9 @@
+package Buoi5.TH2.BaiThucHanhCoBan;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        int i = 5;
+        System.out.println("i++ = " + ++i);
+        System.out.println("i-- = " + --i);
+    }
+}
