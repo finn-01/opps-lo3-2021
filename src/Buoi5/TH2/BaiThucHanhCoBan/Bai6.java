b6293120b81fd3d0e20eb9a4eca1fd538f3f1c10
samukma <nguyensam576@gmail.com>
2021-12-05 16:28:31 +0700

    bth2.thcb.b6

---
+++ src/Buoi5/TH2/BaiThucHanhCoBan/Bai6.java
@@ -0,0 +1,41 @@
+package Buoi5.TH2.BaiThucHanhCoBan;
+
+import java.util.Scanner;
+
+public class Bai6 {
+    public static int sum(int n){
+        long tong = 0;
+        long tich = 1;
+        for(int i = 1; i <= n; i++){
+            tich = tich *i;
+            tong = tong + tich;
+        }
+        return (int) tong;
+    }
+
+    public static int chanLe(int n){
+        int sum= 0;
+        if(n % 2 == 0){
+            for(int i = 0; i <= n; i+=2){
+                sum+=i;
+            }
+        }
+        else {
+            for(int i = 0; i <= n; i+=2){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("N: ");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println(sum(n));
+        System.out.println(chanLe(n));
+    }
+}
